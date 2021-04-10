90087ee29dc01540ab32248b4cdbc3b4efc26f53
mcsf19a020 <mcsf19a037@pucit.edu.pk>
2021-04-10 16:33:11 +0500

    DrawerNavigationApplication Lecture 12 Almost Done

--- Lecture 12/app/src/main/java/com/example/drawernavigationapplication/MainActivity.java
+++ Lecture 12/app/src/main/java/com/example/drawernavigationapplication/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.drawernavigationapplication;
 
+import  androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.toolbar);
+        //getSupportActionBar(toolbar);
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.Openning,R.string.Closing);
+        //actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 }
