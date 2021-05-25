31cf51c83fa4f9758dbeaed17881d3de961f91a3
mcsf19a020 <mcsf19a037@pucit.edu.pk>
2021-05-26 01:34:46 +0500

    Lecture 24-5-2021 Fragment Added

---
+++ Lecture 24-05-2021 Fragments/app/src/main/java/com/example/lecture24_05_2021fragments/ui/login/LoginFragment.java
@@ -0,0 +1,40 @@
+package com.example.lecture24_05_2021fragments.ui.login;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lecture24_05_2021fragments.R;
+
+public class LoginFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
